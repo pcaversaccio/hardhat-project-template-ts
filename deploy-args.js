8cc6052a52586bfeb08e18fb122a2b589d6686e7
Defido Coin <95591037+defido@users.noreply.github.com>
2022-02-24 15:45:08 +1000

    Update deploy-args.js

--- deploy-args.js
+++ deploy-args.js
@@ -1,4 +1,7 @@
 // You may also use deployment arguments in JS if needed
+// For general use, the ts file will work
+// This is a backup file
+
 module.exports = [
     process.env.SETUP_WALLET_ADDRESS,
 ];
