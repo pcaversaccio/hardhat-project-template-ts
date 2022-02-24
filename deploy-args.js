31bb7ac787291994ff43874b584cee4fd77f1847
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2022-02-24 09:46:49 +0100

    Delete deploy-args.js

--- deploy-args.js
+++
@@ -1,7 +0,0 @@
-// You may also use deployment arguments in JS if needed
-// For general use, the ts file will work
-// This is a backup file
-
-module.exports = [
-    process.env.SETUP_WALLET_ADDRESS,
-];
