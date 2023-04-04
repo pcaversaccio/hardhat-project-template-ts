fba90f349f27046935f674d1975e4d72c3bd81be
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-04-04 11:18:31 +0200

    ♻️  cleanup
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/src/Greeter.sol
+++ contracts/src/Greeter.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.19;
 contract Greeter {
     string private greeting;
 
-    constructor(string memory _greeting) {
-        greeting = _greeting;
+    constructor(string memory greeting_) {
+        greeting = greeting_;
     }
 
     function greet() public view returns (string memory) {
         return greeting;
     }
 
-    function setGreeting(string memory _greeting) public {
-        greeting = _greeting;
+    function setGreeting(string memory newGreeting) public {
+        greeting = newGreeting;
     }
 }
