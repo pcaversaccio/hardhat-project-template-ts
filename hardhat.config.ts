14ab29a220b27f783341c12fc1ee00e1c4bc0bda
g. nicholas d'andrea <nick@gnidan.org>
2023-04-10 19:32:44 -0400

    Update @truffle/dashboard-hardhat-plugin to v0.2.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -85,10 +85,6 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    "truffle-dashboard": {
-      url: "http://localhost:24012/rpc",
-      timeout: 0,
-    },
     tenderly: {
       url: `https://rpc.tenderly.co/fork/${process.env.TENDERLY_FORK_ID}`,
     },
