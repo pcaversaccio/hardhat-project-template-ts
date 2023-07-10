c81e53caffe33fe5b780cc417ef048ac8da6e954
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-07-10 23:06:25 +0200

    🔁 Bump `@typescript-eslint` to version `6.0.0`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
     "prettier",
   ],
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import hre, { ethers } from "hardhat";
 
 async function main() {
