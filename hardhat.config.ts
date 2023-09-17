14a76f5368cc4a7fee1b34aaaa58c4b46d448f3b
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-09-17 19:34:59 +0200

    ➕ Add Arthera and Shardeum Testnet Configurations
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -728,6 +728,26 @@ const config: HardhatUserConfig = {
           ? [process.env.LEDGER_ACCOUNT]
           : [],
     },
+    shardeumTestnet: {
+      chainId: 8081,
+      url: process.env.SHARDEUM_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      ledgerAccounts:
+        process.env.LEDGER_ACCOUNT !== undefined
+          ? [process.env.LEDGER_ACCOUNT]
+          : [],
+    },
+    artheraTestnet: {
+      chainId: 10243,
+      url: process.env.ARTHERA_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      ledgerAccounts:
+        process.env.LEDGER_ACCOUNT !== undefined
+          ? [process.env.LEDGER_ACCOUNT]
+          : [],
+    },
   },
   xdeploy: {
     // Change this name to the name of your main contract
@@ -863,6 +883,8 @@ const config: HardhatUserConfig = {
       // For Manta testnet & mainnet
       manta: process.env.MANTA_API_KEY || "",
       mantaTestnet: process.env.MANTA_API_KEY || "",
+      // For Arthera testnet
+      artheraTestnet: process.env.ARTHERA_API_KEY || "",
     },
     customChains: [
       {
@@ -1095,6 +1117,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://pacific-explorer.testnet.manta.network/api",
           browserURL: "https://pacific-explorer.testnet.manta.network",
+        },
+      },
+      {
+        network: "artheraTestnet",
+        chainId: 10243,
+        urls: {
+          apiURL: "https://explorer-test.arthera.net/api",
+          browserURL: "https://explorer-test.arthera.net",
         },
       },
     ],
