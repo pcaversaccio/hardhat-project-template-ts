a24843b93e37b53d2e45be386954a341450d57e9
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-10-29 10:57:35 +0100

    📝 Refine wording on solc versions
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -51,7 +51,7 @@ const config: HardhatUserConfig = {
     sources: "./contracts/src",
   },
   solidity: {
-    // Only use Solidity versions `>=0.8.20` for EVM networks that support the opcode `PUSH0`
+    // Only use Solidity default versions `>=0.8.20` for EVM networks that support the opcode `PUSH0`
     // Otherwise, use the versions `<=0.8.19`
     version: "0.8.19",
     settings: {
