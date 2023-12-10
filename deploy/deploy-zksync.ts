5b71d7d43b5c4b6dd572b491467367694213484c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-12-10 18:10:30 +0100

    🔁 Migrate to `zksync-ethers` Dependency
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- deploy/deploy-zksync.ts
+++ deploy/deploy-zksync.ts
@@ -1,6 +1,6 @@
 // Note that the deployment scripts must be placed in the `deploy` folder for `hardhat deploy-zksync`
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { Wallet } from "zksync2-js";
+import { Wallet } from "zksync-ethers";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 function delay(ms: number) {
