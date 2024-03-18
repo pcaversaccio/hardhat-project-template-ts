3f66443c081c8baf77c3b1c06434e305dacc846c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-03-18 18:12:09 +0100

    🔁 Upgrade Solidity Version to `0.8.25`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- contracts/src/Greeter.sol
+++ contracts/src/Greeter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.25;
 
 contract Greeter {
     string private greeting;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -76,7 +76,7 @@ const config: HardhatUserConfig = {
   solidity: {
     // Only use Solidity default versions `>=0.8.20` for EVM networks that support the opcode `PUSH0`
     // Otherwise, use the versions `<=0.8.19`
-    version: "0.8.24",
+    version: "0.8.25",
     settings: {
       optimizer: {
         enabled: true,
