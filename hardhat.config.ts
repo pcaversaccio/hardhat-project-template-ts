5cf971ed4e0682f4e2307c74eb52f81264e9ea32
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-05-02 18:44:49 +0200

    ➕ Add X Layer Main and Test Networks
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -690,6 +690,24 @@ const config: HardhatUserConfig = {
       accounts,
       ledgerAccounts,
     },
+    seiTestnet: {
+      chainId: 713715,
+      url: vars.get("SEI_TESTNET_URL", "https://evm-rpc-arctic-1.sei-apis.com"),
+      accounts,
+      ledgerAccounts,
+    },
+    xlayerTestnet: {
+      chainId: 195,
+      url: vars.get("XLAYER_TESTNET_URL", "https://testrpc.xlayer.tech"),
+      accounts,
+      ledgerAccounts,
+    },
+    xlayerMain: {
+      chainId: 196,
+      url: vars.get("XLAYER_MAINNET_URL", "https://rpc.xlayer.tech"),
+      accounts,
+      ledgerAccounts,
+    },
   },
   xdeploy: {
     // Change this name to the name of your main contract
@@ -870,6 +888,9 @@ const config: HardhatUserConfig = {
       // For Mode testnet & mainnet
       mode: vars.get("MODE_API_KEY", ""),
       modeTestnet: vars.get("MODE_API_KEY", ""),
+      // For X Layer testnet & mainnet
+      xlayer: vars.get("OKLINK_API_KEY", ""),
+      xlayerTestnet: vars.get("OKLINK_API_KEY", ""),
     },
     customChains: [
       {
@@ -1304,6 +1325,24 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://sepolia.explorer.mode.network/api",
           browserURL: "https://sepolia.explorer.mode.network",
+        },
+      },
+      {
+        network: "xlayer",
+        chainId: 196,
+        urls: {
+          apiURL:
+            "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/XLAYER",
+          browserURL: "https://www.oklink.com/xlayer",
+        },
+      },
+      {
+        network: "xlayerTestnet",
+        chainId: 195,
+        urls: {
+          apiURL:
+            "https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/XLAYER_TESTNET",
+          browserURL: "https://www.oklink.com/xlayer-test",
         },
       },
     ],
