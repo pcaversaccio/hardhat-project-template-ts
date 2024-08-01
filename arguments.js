a7d8d6869b78498322a24ad2c85f55ec2f6428b0
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-08-01 12:25:11 +0200

    🔁 Bump `typescript-eslint` to `8.0.0`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- arguments.js
+++ arguments.js
@@ -2,7 +2,6 @@
 // Required by the Hardhat plugin `hardhat-etherscan`
 // See also here: https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html#complex-arguments
 
-// eslint-disable-next-line no-undef
 module.exports = [
   "Hello, Hardhat!", // The example Greeter.sol file needs a string
 ];
