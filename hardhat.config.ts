e6c6f537638b2c0d334acc9f1629682da2693df8
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-09-24 12:46:13 +0200

    ➕ Add Oasis Sapphire Test and Main Networks
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -845,6 +845,21 @@ const config: HardhatUserConfig = {
       accounts,
       ledgerAccounts,
     },
+    sapphireTestnet: {
+      chainId: 23295,
+      url: vars.get(
+        "SAPPHIRE_TESTNET_URL",
+        "https://testnet.sapphire.oasis.io",
+      ),
+      accounts,
+      ledgerAccounts,
+    },
+    sapphireMain: {
+      chainId: 23294,
+      url: vars.get("SAPPHIRE_MAINNET_URL", "https://sapphire.oasis.io"),
+      accounts,
+      ledgerAccounts,
+    },
   },
   xdeploy: {
     // Change this name to the name of your main contract
@@ -1061,6 +1076,9 @@ const config: HardhatUserConfig = {
       // For 5ireChain testnet & mainnet
       "5ireChain": vars.get("5IRE_CHAIN_API_KEY", ""),
       "5ireChainTestnet": vars.get("5IRE_CHAIN_API_KEY", ""),
+      // For Oasis Sapphire testnet & mainnet
+      sapphire: vars.get("SAPPHIRE_API_KEY", ""),
+      sapphireTestnet: vars.get("SAPPHIRE_API_KEY", ""),
     },
     customChains: [
       {
@@ -1683,6 +1701,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://testnet.5irescan.io/api",
           browserURL: "https://testnet.5irescan.io",
+        },
+      },
+      {
+        network: "sapphire",
+        chainId: 23294,
+        urls: {
+          apiURL: "https://explorer.oasis.io/mainnet/sapphire/api",
+          browserURL: "https://explorer.oasis.io/mainnet/sapphire",
+        },
+      },
+      {
+        network: "sapphireTestnet",
+        chainId: 23295,
+        urls: {
+          apiURL: "https://explorer.oasis.io/testnet/sapphire/api",
+          browserURL: "https://explorer.oasis.io/testnet/sapphire",
         },
       },
     ],
