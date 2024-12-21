41ad340ff0147bffa675ff15208fa56830a2eaa2
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-12-21 12:23:02 +0100

    ➕ Add EVM on Flow Test and Main Networks Configurations
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -980,6 +980,18 @@ const config: HardhatUserConfig = {
       accounts,
       ledgerAccounts,
     },
+    flowTestnet: {
+      chainId: 545,
+      url: vars.get("FLOW_TESTNET_URL", "https://testnet.evm.nodes.onflow.org"),
+      accounts,
+      ledgerAccounts,
+    },
+    flowMain: {
+      chainId: 747,
+      url: vars.get("FLOW_MAINNET_URL", "https://mainnet.evm.nodes.onflow.org"),
+      accounts,
+      ledgerAccounts,
+    },
   },
   xdeploy: {
     // Change this name to the name of your main contract
@@ -1230,6 +1242,9 @@ const config: HardhatUserConfig = {
       // For Sonic testnet & mainnet
       sonic: vars.get("SONIC_API_KEY", ""),
       sonicTestnet: vars.get("SONIC_API_KEY", ""),
+      // For EVM on Flow testnet & mainnet
+      flow: vars.get("FLOW_API_KEY", ""),
+      flowTestnet: vars.get("FLOW_API_KEY", ""),
     },
     customChains: [
       {
@@ -2028,6 +2043,22 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: "https://api-testnet.sonicscan.org/api",
           browserURL: "https://testnet.sonicscan.org",
+        },
+      },
+      {
+        network: "flow",
+        chainId: 747,
+        urls: {
+          apiURL: "https://evm.flowscan.io/api",
+          browserURL: "https://evm.flowscan.io",
+        },
+      },
+      {
+        network: "flowTestnet",
+        chainId: 545,
+        urls: {
+          apiURL: "https://evm-testnet.flowscan.io/api",
+          browserURL: "https://evm-testnet.flowscan.io",
         },
       },
     ],
