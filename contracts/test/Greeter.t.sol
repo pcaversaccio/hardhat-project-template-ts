2f17c54f8d4a45a8283a64861f3e198753f18b3b
Franco Victorio <victorio.franco@gmail.com>
2025-08-05 18:44:49 +0200

    Replace solhint with slippy

--- contracts/test/Greeter.t.sol
+++ contracts/test/Greeter.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.30;
 
-import {Greeter} from "../src/Greeter.sol";
 import {Test} from "forge-std/Test.sol";
+import {Greeter} from "../src/Greeter.sol";
 
 contract GreeterTest is Test {
     Greeter public greeter;
